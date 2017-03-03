95fc1ae964168acf30d8e12a2e6964dd79411583
sophia-lim <sophia.lim@mail.concordia.ca>
2017-03-03 16:41:49 -0500

    Touch Script
    Print touch data and offset on screen

---
+++ TouchScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		//Write data for each touch input
+		foreach (Touch inputTouch in Input.touches) {
+			string message = "";
+			message += "ID: " + inputTouch.fingerId + "\n";
+			message += "Phase: " + inputTouch.phase.ToString() + "\n";
+			message += "TapCount: " + inputTouch.tapCount + "\n";
+			message += "Pos X: " + inputTouch.position.x + "\n";
+			message += "Pos Y: " + inputTouch.position.y + "\n";
+
+			//Offset finger input all across the screen
+			int num = inputTouch.fingerId;
+			GUI.Label(new Rect(0 + 130 * num, 0, 120, 100);
+		}
+	}
+}
