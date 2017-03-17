9a7ec5d98d8cab892950eaf1bcc2d73a2d116012
Sophia L <Sophia L>
2017-03-16 21:23:23 -0400

    Merging changes on same files/Trying to fix conflict

--- genericPrototype/Assets/Scripts/registerInput.cs
+++ genericPrototype/Assets/Scripts/registerInput.cs
@@ -5,48 +5,40 @@ using UnityEngine.UI;
 
 public class registerInput : MonoBehaviour {
 
-    private InputField mainInputField;
-    private string textInput;
-
     // Checks if there is anything entered into the input field.
     // From Unity3D documentation: https://docs.unity3d.com/ScriptReference/UI.InputField-onEndEdit.html
-    void LockInput(InputField input) {
+    public InputField heroNameInput;
+    public Text heroName;
+
+    public InputField emailInput;
+    public static string email;
+
+    public InputField passwordInput;
+    public static string password;
+
+
+    // Checks if there is anything entered into the input field.
+    void LockInput(InputField input, Text textObj) {
         if (input.text.Length > 0) {
             Debug.Log("Text has been entered");
+            textObj.text = input.text;
             Debug.Log(input.text);
         } else if (input.text.Length == 0) {
             Debug.Log("Main Input Empty");
         }
     }
 
-    // Use this for initialization
-    void Start () {
-        mainInputField.onEndEdit.AddListener(delegate { LockInput(mainInputField); });
+    public void Start() {
+        //Adds a listener that invokes the "LockInput" method when the player finishes editing the main input field.
+        //Passes the main input field into the method when "LockInput" is invoked
+        heroNameInput.onEndEdit.AddListener(delegate { LockInput(heroNameInput, heroName); });
+        //emailInput.onEndEdit.AddListener(delegate { LockInput(emailInput, email); });
+        //passwordInput.onEndEdit.AddListener(delegate { LockInput(passwordInput, password); });
+
     }
 
-    //Setter for InputField
-    public void setInput(InputField mainInputField) {
-        this.mainInputField = mainInputField;
-    }
 
-    //Getter for InputField
-    public InputField getInput() {
-        return mainInputField;
-    }
-
-    //Setter for text Input
-    public void setTextInput(string textInput){
-        this.textInput = textInput;
-    }
-
-    //Getter for text Input
-    public string getTextInput() {
-        return textInput;
-    }
-
-	
-	// Update is called once per frame
-	void Update () {
-		
+    // Update is called once per frame
+    void Update () {
 	}
 }
