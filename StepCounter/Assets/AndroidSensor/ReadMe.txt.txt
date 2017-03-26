0161f5e7d56e3c3da009cfc97c2ac1e1af57bffc
CSLAB <cslab@cslabs-mbp.wireless.concordia.ca>
2017-03-26 11:47:49 -0400

    Added pedometer project and test scene

---
+++ StepCounter/Assets/AndroidSensor/ReadMe.txt.txt
@@ -0,0 +1,44 @@
+Android Sensor for Unity3d
+
+To Test this plugin all you have to do is go to Assets/Plugins/Android/SampleAndroidManifest and copy AndroidManifest File to Assets/Plugins/Android
+and build the project on actual Android Device and then test the demo
+
+Note: You need actual Physical Android Device to test this Plugin
+
+Also: Pedometer only work on Android 4.4(Kitkat) API 20 for more information look here in this link
+http://developer.android.com/reference/android/hardware/Sensor.html#STRING_TYPE_STEP_COUNTER 
+
+
+
+How to run the demo
+
+1. go to Assets/Plugins/Android/SampleAndroidManifest copy the Android Manifest file
+2. paste it to Assets/Plugins/Android directory
+3. go to Asset/AndroidSensor/Scenes and then open the demo scene
+4. go to file menu, build settings and then add Demo scene to scenes in build by clicking add current
+5. go to file menu, build settings and switch platform to Android
+6. go to file menu, build settings and then click player settings and look for other settings 
+and then change the bundle indentifier with your bundle indentifier for example: com.mystudio.mygame
+7. go to file menu, build settings and then click build and run
+8. test the demo
+
+
+
+
+API details
+http://developer.android.com/guide/topics/manifest/uses-sdk-element.html#ApiLevels
+
+For more Information
+http://www.gigadrillgames.com/2015/08/09/android-sensor-for-unity3d/
+
+For Tutorials
+http://www.gigadrillgames.com/2015/08/09/android-sensor-tutorials/
+
+Frequently Asked Questions
+http://www.gigadrillgames.com/2015/08/09/android-sensor-faq-frequently-asked-questions/
+
+For Questions just email us at gigadrillgames@gmail.com
+
+if you like this plugin ,you can also try Android Ultimate Plugin
+https://www.assetstore.unity3d.com/en/#!/content/38872
+
---
+++ StepCounter/Assets/AndroidSensor/Scripts/Main/PedometerPlugin.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PedometerPlugin : MonoBehaviour {
+	
+	private static PedometerPlugin instance;
+	private static GameObject container;
+	private static AUPHolder aupHolder;
+	
+	#if UNITY_ANDROID
+	private static AndroidJavaObject jo;
+	#endif	
+	
+	public bool isDebug =true;
+	
+	public static PedometerPlugin GetInstance(){
+		if(instance==null){
+			container = new GameObject();
+			container.name="PedometerPlugin";
+			instance = container.AddComponent( typeof(PedometerPlugin) ) as PedometerPlugin;
+			DontDestroyOnLoad(instance.gameObject);
+			aupHolder = AUPHolder.GetInstance();
+			instance.gameObject.transform.SetParent(aupHolder.gameObject.transform);
+		}
+		
+		return instance;
+	}
+	
+	private void Awake(){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo = new AndroidJavaObject("com.gigadrillgames.androidsensor.pedometer.PedometerPlugin");
+		}
+		#endif
+	}
+	
+	/// <summary>
+	/// Sets the debug.
+	/// 0 - false, 1 - true
+	/// </summary>
+	/// <param name="debug">Debug.</param>
+	public void SetDebug(int debug){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("SetDebug",debug);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+
+	public void Init(){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("init");
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+	public void SetCallbackListener(Action <int>OnStepCount,Action OnStepDetect){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			IPedometerCallback ipedometerCallback = new IPedometerCallback();
+			ipedometerCallback.OnStepCount = OnStepCount;
+			ipedometerCallback.OnStepDetect = OnStepDetect;
+			jo.CallStatic("setCallbackListener",ipedometerCallback);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+	/// <summary>
+	/// Sets the step to always start at zero when app is started.
+	/// </summary>
+	/// <param name="val">If set to <c>true</c> value.</param>
+	public void SetAlwaysStartAtZero(bool val){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("setAlwaysStartAtZero",val);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+	/// <summary>
+	/// Resets the total step. because steps is counted when the device booted to manually reset it restart the device
+	/// </summary>
+	public void ResetTotalStep(){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("resetTotalStep");
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+	public void RegisterSensorListener(SensorDelay sensorDelay){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("registerSensorListener",(int)sensorDelay);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+	
+	public void RemoveSensorListener(){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("removeSensorListener");
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+
+	
+	/// <summary>
+	/// Shows the native alert popup.
+	/// </summary>
+	/// <param name="title">Title.</param>
+	/// <param name="message">Message.</param>
+	public void ShowAlertPopup(String title,String message){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.CallStatic("showNativePopup",title,message);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+	
+	
+	public void ShowMessage(string message){
+		#if UNITY_ANDROID
+		if(Application.platform == RuntimePlatform.Android){
+			jo.Call("ShowMessage", message);
+		}else{
+			Message("warning: must run in actual android device");
+		}
+		#endif
+	}
+	
+	private void Message(string message){
+		if(isDebug){
+			Debug.LogWarning(message);
+		}
+	}
+}
