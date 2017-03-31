418dcc0879a55ee3665924c8de399ec5744d4902
Sophia L <Sophia L>
2017-03-30 21:49:56 -0400

    Export/Import edited pedometer package into main pack

---
+++ genericPrototype/Assets/AndroidSensor/Scripts/Callback/IPedometerCallback.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class IPedometerCallback :  AndroidJavaProxy {
+	
+	public Action <int>OnStepCount;
+	public Action OnStepDetect;
+	
+	public IPedometerCallback() : base("com.gigadrillgames.androidsensor.pedometer.IPedometerCallback") {}
+
+	void onStepCount(int count){
+		OnStepCount(count);
+	}
+
+	void onStepDetect(){
+		OnStepDetect();
+	}
+}
