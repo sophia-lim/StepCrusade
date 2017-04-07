cea9e6226b48e3830fe8b298486323126fd7391c
CSLAB <cslab@cslabs-mbp.wireless.concordia.ca>
2017-04-07 16:11:14 -0400

    Interactable ghost scripts/classes

--- genericPrototype/Assets/Scripts/ActionItem.cs
+++ genericPrototype/Assets/Scripts/ActionItem.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionItem : MonoBehaviour {
+//Default action code
+public class ActionItem : Interactable {
+
+	//Cant override action
+	public override void Interact(){
+		Debug.Log ("Interacting with base action object.");
+	}
 
 	// Use this for initialization
 	void Start () {
---
+++ genericPrototype/Assets/Scripts/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour {
+
+	public NavMeshAgent playerAgent;
+
+	public virtual void MoveToInteraction(NavMeshAgent playerAgent){
+		this.playerAgent = playerAgent;
+		playerAgent.stoppingDistance = 3f;
+		playerAgent.destination = this.transform.position;
+		Interact ();
+	}
+
+	public virtual void Interact(){
+		Debug.Log ("Interacting with Interactable Item.");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
