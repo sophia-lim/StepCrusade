b0b512c6ba0a3ea29de59e1a5dd4e07be52b7656
Sophia L <Sophia L>
2017-04-10 09:09:51 -0400

    Issue: db structure creation

---
+++ genericPrototype/Assets/Scripts/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User {
+
+    public string heroName;
+    public string email;
+
+    public User() {
+    }
+
+    public User(string heroName, string email) {
+        this.heroName = heroName;
+        this.email = email;
+    }
+    
+
+}
