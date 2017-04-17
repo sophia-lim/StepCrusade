78aab97207ba5911f55ef0f2f8d2bfa12a1b2d5d
William McGuinness <willmjmcg@gmail.com>
2017-04-17 00:28:49 -0400

    FUNCTIONING POWERUP BUTTON
    Power up button works in dial menu.
    Goes to shop.

---
+++ genericPrototype/Assets/Scripts/PowerUpButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PowerUpButton : MonoBehaviour {
+
+	public Button powerUpShop;
+
+	void Start() 
+	{
+		Button btn = powerUpShop.GetComponent<Button>();
+		btn.onClick.AddListener(goToPowerUpScene);
+	}
+
+	public void goToPowerUpScene ()
+	{
+		SceneManager.LoadScene ("Shop_PowerUp");
+	}
+
+}
